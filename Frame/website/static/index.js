c1d6251223a91df3425414fb34b1877c7702bae1
Jesse <jesseforney8@gmail.com>
2023-01-09 03:08:55 -0600

    moved most of js to index.js instead of on html page

--- Frame/website/static/index.js
+++ Frame/website/static/index.js
@@ -25,3 +25,50 @@ window.onclick = function(event) {
     modal_filter.style.display = "none";
   }
 }
+
+// name drop down
+
+function dropdown() {
+  document.getElementById("dropdowninfo").classList.toggle("show");
+}
+
+// removes user from org
+
+function remove(email){
+  fetch("/removeorg", {
+            method: "POST",
+            body: JSON.stringify({ email: email}),
+          }).then((_res) => {
+              window.location.href = "/members";
+          });
+        
+}
+
+// changes role
+
+function role1(email, id){
+  v = document.getElementById(id).value;
+  
+  
+      fetch("/changerole", {
+                method: "POST",
+                body: JSON.stringify({ email: email, role: v}),
+              }).then((_res) => {
+                  window.location.href = "/members";
+              });
+            
+  }
+
+  //creates group
+
+  function addgroup(user){
+    var group = document.getElementById("grp")
+    console.log(group.value)
+        fetch("/addgroup", {
+                  method: "POST",
+                  body: JSON.stringify({ group: group.value, email: user}),
+               }).then((_res) => {
+                    window.location.href = "/members";
+                });
+              
+    }

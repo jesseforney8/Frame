90c540ee73e374d1746923352e6c8d6a5e187a22
Jesse <jesseforney8@gmail.com>
2023-01-09 03:19:01 -0600

    moved all js to index.js

--- Frame/website/static/index.js
+++ Frame/website/static/index.js
@@ -59,7 +59,7 @@ function role1(email, id){
             
   }
 
-  //creates group
+//creates group
 
   function addgroup(user){
     var group = document.getElementById("grp")
@@ -69,6 +69,109 @@ function role1(email, id){
                   body: JSON.stringify({ group: group.value, email: user}),
                }).then((_res) => {
                     window.location.href = "/members";
-                });
-              
+                });   
     }
+
+// makes ticket editable and then submits new ticket info into db
+
+var title = document.getElementById("title");
+var body = document.getElementById("body");
+var owner = document.getElementById("owner");
+var submitter = document.getElementById("submitter");
+var type = document.getElementById("type");
+var urgency = document.getElementById("urgency");
+const edit_button = document.getElementById("edit-button");
+const end_button = document.getElementById("end-button");
+var id = document.getElementById("id");
+    
+    
+    
+    
+edit_button.addEventListener("click", function() {
+        
+      title.contentEditable = true;
+      title.style.backgroundColor = "#dddbdb";
+    
+      body.contentEditable = true;
+      body.style.backgroundColor = "#dddbdb";
+    
+      owner.contentEditable = true;
+      owner.style.backgroundColor = "#dddbdb";
+    
+      submitter.contentEditable = true;
+      submitter.style.backgroundColor = "#dddbdb";
+    
+      type.contentEditable = true;
+     type.style.backgroundColor = "#dddbdb";
+    
+     urgency.contentEditable = true;
+     urgency.style.backgroundColor = "#dddbdb";
+
+    document.getElementById("end-button").hidden = false;
+    
+    } );
+    
+end_button.addEventListener("click", function() {
+      title.contentEditable = false;
+      body.contentEditable = false;
+      owner.contentEditable = false;
+      submitter.contentEditable = false;
+      type.contentEditable = false;
+      urgency.contentEditable = false;
+      document.getElementById("end-button").hidden = true;
+    
+    
+    
+function editTicket(ticketId) {
+        fetch("/ticket", {
+          method: "POST",
+          body: JSON.stringify({ ticketId: ticketId, title: title.textContent, body: body.textContent, owner: owner.textContent, submitter: submitter.textContent, type: type.textContent, urgency: urgency.textContent}),
+        }).then((_res) => {
+            window.location.href = "/ticket";
+        });
+      }
+      editTicket(id.textContent);
+    } )
+
+
+//deletes ticket
+
+function deleteTicket(ticketId) {
+      fetch("/delete-ticket", {
+        method: "POST",
+        body: JSON.stringify({ ticketId: ticketId }),
+      }).then((_res) => {
+        window.location.href = "/tickets";
+      });
+    }
+
+//going into more detail on selected ticket
+
+function GotoTicket(ticketId) {
+      fetch("/tickets", {
+        method: "POST",
+        body: JSON.stringify({ ticketId: ticketId}),
+      }).then((_res) => {
+          window.location.href = "/ticket";
+      });
+    }
+  
+// silter search function, work in progess
+
+function add_search() {
+let btnval = document.getElementById("searchowner");
+let finalsearch = document.getElementById("finalsearch");
+let filterinput = document.getElementById("filterinput");
+
+finalsearch.value = btnval.textContent + ": " + filterinput.value;
+
+}
+
+function add_search1() {
+let btnval = document.getElementById("searchgroup");
+let finalsearch = document.getElementById("finalsearch");
+let filterinput = document.getElementById("filterinput");
+
+finalsearch.value = btnval.textContent + ": " + filterinput.value;
+
+}
